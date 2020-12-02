c389859980b76c652c70a3418ba5aff257715a1d
Ellie Kelsch <ebkelsch@gmail.com>
2020-12-02 14:30:58 +0000

    :recycle: fix conditionals

--- dayTwo/partOne.js
+++ dayTwo/partOne.js
@@ -2,23 +2,30 @@ const passwords = require('./puzzleInput')
 
 const matchesRule = (rule, letters) => {
   const ruleInts = rule.split('-')
-  letters.length >= ruleInts[0] && letters.length <= ruleInts[1] ? true : false
+  if (letters.length >= ruleInts[0] && letters.length <= ruleInts[1]) {
     return true
+  } else {
+    return false
+  }
 }
 
 const isValidPassword = (rule, targetLetter, password) => {
   const matchingLetters = password.filter(letter => letter === targetLetter)
-  matchingLetters.length === 0 ? matchesRule(rule, matchingLetters) : false
+  if (matchingLetters.length === 0) {
+    return false
+  } else {
+    return matchesRule(rule, matchingLetters)
+  }
 }
 
-const validatePasswords = (passwordList) => {
-  const validPasswords = passwordList.filter(listItem => {
-    const split = listItem.split(' ')
+const validatePasswords = (list) => {
+  const validPasswords = list.filter(list => {
+    const split = list.split(' ')
     const rule = split[0]
     const targetLetter = split[1].split('')[0]
     const password = split[2].split('')
     if (isValidPassword(rule, targetLetter, password)) {
-      return listItem
+      return list
     }
   })
   return validPasswords.length
